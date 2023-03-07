7e9ee1691956931535f3f04c7f4ea29aae0e2b5d
mehedi1256 <mehedihassanshovospirt@gmail.com>
2023-03-07 13:29:10 +0600

    committed

---
+++ admin/php_files/logout.php
@@ -0,0 +1,11 @@
+<?php
+    include 'config.php';
+    /* Start the session */
+    session_start();
+    /* remove all session variables */
+    session_unset(); 
+    /* destroy the session */
+    session_destroy();
+
+    header("location:{$base_url}admin/index.php");
+?>
